31c3bd88933e39ce3ed05b71dd9d2025019e9da9
ojoaox <souzajoaovictor555@gmail.com>
2026-06-06 20:51:10 -0300

    fix: correct spelling in HTML and update mobile menu icon to SVG

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   menuToggle.addEventListener('click', () => {
     const isOpen = mobileMenu.classList.toggle('hidden');
     // Change icon
-    menuToggle.innerHTML = isOpen ? '<i class="fa-solid fa-bars"></i>' : '<i class="fa-solid fa-xmark"></i>';
+    menuToggle.innerHTML = isOpen ? '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>' : '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>';
   });
 
   // Parallax background for hero
